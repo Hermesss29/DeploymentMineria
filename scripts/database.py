7a62491ab8d05420a51ff058b3b4623ac75a00d9
root <root@Marsss.>
2026-03-04 21:55:20 -0500

    fix: conexión a Supabase

--- scripts/database.py
+++ scripts/database.py
@@ -6,14 +6,21 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import logging
 
 load_dotenv()
-
 logger = logging.getLogger(__name__)
 
-DB_HOST     = os.getenv('DB_HOST')
-DB_PORT     = os.getenv('DB_PORT')
-DB_USER     = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_NAME     = os.getenv('DB_NAME')
+try:
+    import streamlit as st
+    DB_HOST     = st.secrets["DB_HOST"]
+    DB_PORT     = st.secrets["DB_PORT"]
+    DB_USER     = st.secrets["DB_USER"]
+    DB_PASSWORD = st.secrets["DB_PASSWORD"]
+    DB_NAME     = st.secrets["DB_NAME"]
+except:
+    DB_HOST     = os.getenv('DB_HOST', 'db.ezchuppodybddecvzxht.supabase.co')
+    DB_PORT     = os.getenv('DB_PORT', '5432')
+    DB_USER     = os.getenv('DB_USER', 'postgres')
+    DB_PASSWORD = os.getenv('DB_PASSWORD', 'Mars2905@123')
+    DB_NAME     = os.getenv('DB_NAME', 'postgres')
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
